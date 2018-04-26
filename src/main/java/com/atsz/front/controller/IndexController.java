84a8fd64d0577a3db2430d5bb1a386ff3bd07a5a
liangf <472383881@qq.com>
2018-04-26 15:57:42 +0800

    first

---
+++ src/main/java/com/atsz/front/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.atsz.front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+//    /**
+//     * 显示首页
+//     */
+//    @RequestMapping(value="index.html")
+//    public String showIndex(){
+//       return "index";
+//    }
+}
